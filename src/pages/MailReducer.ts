2e6c37e79702ef0bd86d44bb24ad83ff6365a4bd
Antonio Santiago <asantiago@Antonios-MacBook-Pro.local>
2021-04-05 09:17:52 -0500

    - Added first validator in SET_INBOX_DATA to check for spam in the messages

--- src/pages/MailReducer.ts
+++ src/pages/MailReducer.ts
@@ -1,5 +1,34 @@
 import { MailPageState } from '../models/MailPageState';
 import { MailPageActionTypes, SET_INBOX_DATA, SET_SEARCHBAR_TEXT } from '../models/actions/MailPage.actions';
+import { Messages } from '../models/Messages';
+import { Mail } from '../models/Mail';
+
+const containsSpam = (mailExtract: string) => {
+  const SPAM = 'SPAM';
+  if (mailExtract.toUpperCase().includes(SPAM)) {
+    return true;
+  }
+  return false;
+};
+
+const removeMail = (mailList: Mail[], mailNumber: Mail): Mail[] => {
+  const index = mailList.indexOf(mailNumber);
+  if (index > -1) {
+    mailList.splice(index, 1);
+  }
+  return mailList;
+}
+
+const processMessages = (messages: Messages): Messages => {
+  for (let i = 0; i < messages.mailList.length; i++) {
+    const body = messages.mailList[i].body;
+    const subject = messages.mailList[i].subject;
+    if (containsSpam(body) || containsSpam (subject)) {
+      removeMail(messages.mailList, messages.mailList[i]);
+    }
+  }
+  return messages;
+};
 
 export const reducer = (
   state: MailPageState,
@@ -13,9 +42,10 @@ export const reducer = (
       };
     }
     case SET_INBOX_DATA: {
+      const inbox = processMessages(action.messages);
       return {
         ...state,
-        inbox: action.messages,
+        inbox,
       };
     }
     default: return state;
